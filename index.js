de3d087daf56fdf6a0d79cf17cd1cd412c68df30
Sophie Gu <sophieqgu@gmail.com>
2021-01-20 04:29:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,26 @@
 const returnFirstTwoDrivers = function(array_of_drivers) {
   return array_of_drivers.slice(0, 2);
 }
+
+const returnLastTwoDrivers = function(array_of_drivers) {
+  return array_of_drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(times) {
+  return function(fare) {
+    for (let i = 0; i < times; i++) {
+      fare *= fare;
+    }
+    return fare;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)(fare);
+
+const fareTripler = createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(array_of_drivers, selectingDrivers) {
+  
+}
